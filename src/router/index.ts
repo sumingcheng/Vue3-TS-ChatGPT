767e86ba1dfeff1d69a9ebc25be84ed95ce4b7d9
Sumingcheng <fanxiaohaohaha@qq..com>
2023-03-29 20:16:00 +0800

    history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-    history: createWebHashHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
