690aca23a25e6e8436c1210b4a6d217e374dcfc8
范晓豪 <fanxiaohaohaha@qq.com>
2023-04-14 17:43:31 +0800

    增加路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: () => import("@/views/home.vue"),
-        },
-        {
-            path: "/new",
-            name: "new",
-            component: () => import("@/views/new.vue"),
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("@/views/home.vue"),
+    },
+    {
+      path: "/new",
+      name: "new",
+      component: () => import("@/views/new.vue"),
+    },
+    // 其他路由配置...
+    {
+      path: '/:catchAll(.*)', // 使用动态参数和通配符
+      component: () => import("@/views/home.vue"),
+    }
+  ],
 })
 
 export default router
