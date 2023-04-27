53dd0eb1bd5e8643d45b6ba337c138b6ea4799ee
范晓豪 <fanxiaohaohaha@qq.com>
2023-04-27 16:03:34 +0800

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "./",
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 3456 // 指定端口号
   },
