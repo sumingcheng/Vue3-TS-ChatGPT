80a79bd2a6980a12223b0580d38f58047ff2d04e
范晓豪 <fanxiaohaohaha@qq.com>
2023-06-14 17:28:03 +0800

    fix：删除冗余，更新模型

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import {createRouter, createWebHashHistory} from "vue-router"
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -8,12 +8,6 @@ const router = createRouter({
       name: "home",
       component: () => import("@/views/home.vue"),
     },
-    {
-      path: "/new",
-      name: "new",
-      component: () => import("@/views/new.vue"),
-    },
-    // 其他路由配置...
     {
       path: '/:catchAll(.*)', // 使用动态参数和通配符
       component: () => import("@/views/home.vue"),
